e9d273ea1d545f19acde9422ccfda655060f54ff
kleberfort <kleberfort@gmail.com>
2022-03-23 10:36:43 -0300

    first commit

---
+++ src/br/com/fort/kleber/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.fort.kleber;
+
+import br.com.fort.kleber.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+//        System.out.println("Hello World");
+
+
+
+    }
+}
